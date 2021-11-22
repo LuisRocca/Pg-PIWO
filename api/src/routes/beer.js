2c50ef81377dbe4e74895b0df35b0bfb3aa10581
pabmena <you@example.com>
2021-11-22 19:31:22 -0300

    check BD2

--- api/src/routes/beer.js
+++ api/src/routes/beer.js
@@ -1,33 +1,44 @@
 const { Router } = require("express");
+const { Beer } = require("../db.js");
 const { Op } = require("sequelize");
-const { User, Beer } = require("../db.js");
+const axios = require("axios");
 
 const router = Router();
 
-router.post("/", async (req, res) => {
-    try {
-   const { name, username, email, age, beers } = req.body;
-   const userAdd = await User.create({
-      
-      name: name,
-      username: username,
-      email: email,
-      age: age,
+const getApiBeer = async () => { 
+  const beers = await Beer.findAll({
+    attributes: ["id", "name", "image", "price", "stock", "impresion", "aroma", "flavor", "IBU", "AVB", "history", "ingredients"],
+  });
+console.log("beers", beers);
+
+  if (!beers.length) {
+    var allBeer = await axios.get("https://raw.githubusercontent.com/gthmb/bjcp-2015-json/master/json/styleguide-2015.json");
+    allBeer = allBeer.data 
+
+    console.log("allBeer", allBeer.data);
+    allBeer = allBeer.styleguide.map((el) => {
+
+      return {
+            id: el.cca2,
+            name: el.name,
+            image: el.image,  
+            price: el.price,
+            stock: el.stock,
+            impresion: el.impresion,
+            aroma: el.aroma,
+            flavor: el.flavor,
+            IBU: el.IBU,
+            AVB: el.AVB,
+            history: el.history,
+            ingredients: el.ingredients,
+      }
     });
-  
-    for (const i of beers) {
-      const beer = await Beer.findOne({
-        where: {
-          id: i,
-        },
-      });
-  
-    beer.addUser(userAdd);
-  }
-    res.json(activityAdd);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error" });
-  }});
+
+
+    await Beer.bulkCreate(allBeer);
+    return allBeer;
+  } else {
+    return beers
+  }};
 
   module.exports = router;
