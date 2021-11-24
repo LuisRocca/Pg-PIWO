5dc8164d5959c8e92433e2e9fb684c881f444fd4
luisRocca <luis.rocca96@gmail.com>
2021-11-24 12:11:35 -0500

    una ruta PUT que MODIFICA el REVIEW por si cambias de parecer

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,64 +10,13 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/review', reviewRouter);
 router.use('/beers', beerRouter);
 router.use('/users', userRouter);
+
 // router.use('/')
-// router.use('/review', reviewRouter);
-const { conn} = require("../db");
-const { User, Beer, Review } = conn.models;
 
 
-// {
-//   "commentary" : "una mierda esta cerveza",
-//   "calification" : "1"
-// }
-
-router.post('/users/beer/:idBeer/user/:idUser', (req, res) => {
-    const {idBeer, idUser} = req.params
-    const {commentary , calification} = req.body
-
-  console.log(idBeer, idUser, commentary, calification )
-// if ( idUser || idBeer) {
-  Beer?.findByPk(idBeer)
-.then((beer) => {
-  console.log("linea 32", beer)
-   beer.addUsers(idUser)
-
-  .then((newRew)=>{ 
-    console.log("linea 38 <--->" , newRew)
-    Review.update({commentary,calification},{
-     where:{userId:idUser,beerId:idBeer}})
-    .then(rev=>{
-      res.status(201).json(rev)
-    })
-   })
-})
-// }
-})
-
-  
-// Beer.findByPk(idBeer)
-// .then((beer) => {
-//   console.log("linea 32", beer)
-//    beer.addUsers(idUser)
-  
-//   .then((newRew)=>{ 
-//     Review.update({commentary,calification},{
-//      where:{userId:idUser,beerId:idBeer}})
-//     .then(rev=>{
-//       res.status(201).json(rev)
-//     })
-//    })
-//     .catch((err) => {
-//       console.log(err)
-//       res.status(400).send(err)
-//        })
-//      })
-//   .catch((err) => {
-//     console.log(err, "sss=>")
-//     res.status(400).send(err)
-//      })
 
 module.exports = router;
  
--- api/src/routes/review.js
+++ api/src/routes/review.js
@@ -1,11 +1,60 @@
 const { Router } = require("express");
-const { Sequelize } = require('sequelize');
-const { User, Beer, Review } = require('../db.js');
+const { conn } = require("../db");
+const { User, Beer, Review } = conn.models;
 const router = Router();
 
-// ruta ---> localhost:3001/review/beer/idBeer/user/iduser <--CREANDPO COMENTARIOS
-// router.post('/', (req, res) => {
-//     const {idBeer, idUser} = req.params
-//     const {commentary , calification} = req.body
-  
-//   })
+// ruta ---> localhost:3001/review/beer/idBeer/user/idUser <--CREANDPO COMENTARIOS
+// { objeto esperado! 
+//   "commentary" : "una mierda esta cerveza",
+//   "calification" : "1"
+// }
+router.post("/beer/:idBeer/user/:idUser", (req, res) => {
+  const { idBeer, idUser } = req.params;
+  const { commentary, calification } = req.body;
+
+  Beer.findByPk(idBeer).then((beer) => {
+    //  console.log(beer, "linea 13 <-----")
+    beer
+      .addUsers(idUser)
+
+      .then((newRew) => {
+        // console.log("linea 38 <--->" , newRew)
+        Review.update(
+          { commentary, calification },
+          {
+            where: { userId: idUser, beerId: idBeer },
+          }
+        ).then((rev) => {
+          res.status(201).json(rev);
+        });
+      }).catch((err) => {
+              console.log(err , "este es el segundo then --> error")  
+              res.status(400).send(err)
+              })
+             }) .catch((err) => {
+                console.log(err, "este es el primer then")
+                res.status(400).send(err)
+                 })
+  });
+
+  // Ruta --> MODIFICACION DEL REVIEW
+
+  router.put( '/beer/:idBeer/user/:idUser', (req , res) =>{
+    const { idBeer, idUser } = req.params;
+    const { commentary, calification } = req.body;
+
+    Review.update({calification, commentary},{
+        where: {
+            beerId: idBeer , 
+            userId: idUser
+        }
+    }) .then((newData)=>{
+        res.status(200).send(newData)
+    }) .catch((error)=>{
+        console.log(error)
+        res.status(400).send(error)
+    })
+
+  });
+
+module.exports = router;
