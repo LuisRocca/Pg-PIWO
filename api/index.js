ba4a62257589ded13c7b880a0824c65dc3e28297
Bruno Sentinelli <brunosentinelli@gmail.com>
2021-11-23 21:16:54 -0300

    empezamos back falta front

--- api/index.js
+++ api/index.js
@@ -18,11 +18,17 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { conn,Beer} = require('./src/db.js');
+const {showAll} = require('./src/methods/index.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => { // este false es para que no guarde la data!
-  server.listen(3001, () => {
+  server.listen(3001,async () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
+    const beers = await showAll();
+    Beer.bulkCreate(
+      beers
+    )
   });
 });
+
--- api/src/routes/beer.js
+++ api/src/routes/beer.js
@@ -5,17 +5,43 @@ const {showAll} = require("../methods/index.js");
 
 const router = Router();
 
+
 router.get("/", async (req, res) => {
+  const {name} = req.query
+  const beersT = await showAll()
   try {
-    const beers = await showAll();
-    Beer.bulkCreate(
-      beers
-    )
-    res.json(beers)
+    if(name){
+      const byName = beersT.filter(n => n.name.toLowerCase().includes(name.toLowerCase()));
+      byName.length ? 
+     res.status(200).send(byName) :
+      res.status(404).send('no se ha encontrado ninguna cerveza')
+     }
+   
+    
+    // const beers = await showAll();
+    // Beer.bulkCreate(
+    //   beers
+    //   )
+    // res.json(beers)
   }
   catch (err) {
     console.log(err);
   }
+  
 })
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id
+  const beersAll = await showAll()
+  if(id){
+    const beersId = beersAll.filter(i => i.ID === id)
+    beersId.length ? res.status(200).send(beersId) :
+    res.status(404).send('id no valido')
+  }
+})
+
+
+
+
 
   module.exports = router;
