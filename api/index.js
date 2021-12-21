d22ed84a330612a31b82cede35ff9121cdbebb63
luisRocca <luis.rocca96@gmail.com>
2021-12-21 17:33:45 -0500

    nojoda

--- api/index.js
+++ api/index.js
@@ -4,10 +4,11 @@ const {showAll, getCategories} = require('./src/methods/index.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => { // este false es para que no guarde la data!
-  server.listen(process.env.PORT, async () => {
+  server.listen(process.env.PORT || 3001, async () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
+    var port = server.address().port;
     const categories = await getCategories();
     const beers = await showAll();
-    console.log('%listos')
+    console.log('%listos', port)
   });
 });
